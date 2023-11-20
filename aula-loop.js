430d9497e0c3e392cd8aa9359b9ab15cec3485e0
Daniel Luiz <loureirodlg@gmail.com>
2023-11-20 20:30:55 -0300

    usando for para looping

---
+++ aula-loop.js
@@ -0,0 +1,13 @@
+const nomesDeEstudante = ['jose', 'joao', 'maria', 'joana', 'andre'];
+
+for (let i = 0; i < 5; i++) {
+
+    console.log(nomesDeEstudante[i])
+}
+// let indice = 0;
+
+// while (indice < nomesDeEstudante.length) {
+
+//     console.log(nomesDeEstudante[indice]);
+//     indice++;
+// }
