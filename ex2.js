5f3d07a57c4c94bc9d6d42b3c615c134d9a89fca
Daniel Luiz <loureirodlg@gmail.com>
2023-11-20 21:20:09 -0300

    somando numeros da arrays

---
+++ ex2.js
@@ -0,0 +1,9 @@
+const numeros = [0, 3, 5, 6, 9];
+
+let a = 0;
+
+while (a < numeros.length) {
+    console.log(numeros[a]);
+    a++;
+
+}
---
+++ ex3.js
@@ -0,0 +1,9 @@
+const numeros = [10, 20, 30, 40, 50];
+
+let soma = 0
+
+for (let i = 0; i < numeros.length; i++) {
+    soma = soma + numeros[i];
+
+}
+console.log(soma)
