01bc0d8fceccf7210830a2e80d73c46cdf7c8568
Daniel Luiz <loureirodlg@gmail.com>
2023-11-20 22:47:27 -0300

    usando for tradicional

--- ex5.js
+++ ex5.js
@@ -1,8 +1,12 @@
-const numero = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
-
-let sub = 0;
-while (sub < numero.length) {
-    console.log(numero[sub]);
-    sub++;
-
+for (let i = 10; i >= 0; i--) {
+    console.log(i)
 }
+
+// const numero = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+
+// let sub = 0;
+// while (sub < numero.length) {
+//     console.log(numero[sub]);
+//     sub++;
+
+// }
